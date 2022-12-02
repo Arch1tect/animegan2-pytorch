810d360aa9af9cad1e706f177511f9b3e82a7a2a
David Shen <swotong@Davids-MacBook-Pro.local>
2022-12-02 11:27:46 -0800

    working

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
 const express = require("express")
 const fs = require("fs")
-
+const path = require("path")
 const app = express()
+app.use(express.json())
 const spawn = require("child_process").spawn
 
 //Define request response in root URL (/)
-app.get("/", async (req, res) => {
-  const url =
-    "https://firebasestorage.googleapis.com/v0/b/topic-chat-30112.appspot.com/o/users%2F1jH4NLL94QPSNXnFW1s5cWMMFXP2%2Fprofile%2F05-140153_739.jpeg?alt=media&token=99450ee8-a836-4b33-9bc3-1b4d29ad2ac8"
-  // TODO get url from request
-  // File name doesn't really matter, we can use user id
-  const userId = "uuid-123"
-
-  const response = await fetch(url)
+app.post("/api/v1/animate-photo", async (req, res) => {
+  const { imageUrl, userId } = req.body
+  const response = await fetch(imageUrl)
   const arrayBuffer = await response.arrayBuffer()
-  const dateStr = new Date().toISOString().split("T")[0]
-  fs.mkdirSync(`animegan/input/${dateStr}`, { recursive: true })
-  fs.mkdirSync(`animegan/output/${dateStr}`, { recursive: true })
+
   const fileName = `${userId}.jpg`
-  const inputFilePath = `animegan/input/${dateStr}/${fileName}`
-  const outputFilePath = `animegan/output/${dateStr}/${fileName}`
+  const inputFilePath = `animegan/input/${fileName}`
+  const outputFilePath = `animegan/output/${fileName}`
   fs.writeFileSync(inputFilePath, Buffer.from(arrayBuffer))
-  console.log("hi")
 
   const pythonProcess = spawn("python3", [
     "./animegan/main.py",
@@ -39,7 +32,13 @@ app.get("/", async (req, res) => {
   pythonProcess.stdout.on("data", (result) => {
     // Do something with the data returned from python script
     console.log(result.toString())
-    res.send("data")
+    app.use(express.json())
+    // res.sendFile(`/app/${outputFilePath}`)
+    res.sendFile(outputFilePath, { root: __dirname }, (err) => {
+      fs.unlinkSync(path.join(__dirname, inputFilePath))
+      fs.unlinkSync(path.join(__dirname, outputFilePath))
+    })
+
     pythonCompleted = true
   })
   pythonProcess.on("exit", function () {
